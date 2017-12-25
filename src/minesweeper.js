b2d0430ebc83864265ec7ca5d9ff192e57365325
Shaul Yecheskel <shauly@gmail.com>
2017-12-25 15:14:49 -0600

    committed day 10 exercise

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,19 +1,49 @@
-const board = [
-  [' ',' ',' '],
-  [' ',' ',' '],
-  [' ',' ',' ']
-];
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  const board = [];
 
-function printBoard() {
-  console.log('Current Board:');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    const row = [];
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
 };
 
-printBoard();
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  const board = [];
 
-board[0][1] = '1';
-board[1][2] = 'B';
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    const row = [];
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
 
-printBoard();
+  let numberOfBombsPlaced = 0;
+
+  while (numberOfBombsPlaced < numberOfBombs) {
+    // This code has the potential to place bombs on top of bombs, this will be fixed with control flow.
+    const randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+  }
+
+  return board;
+};
+
+const printBoard = board => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+let playerBoard = generatePlayerBoard(3, 3);
+let bombBoard = generateBombBoard(3, 3, 3);
+console.log('Player Board:');
+printBoard(playerBoard);
+console.log('Bomb Board:');
+// bombBoard will sometimes have less bombs than specified due to the previously-mentioned missing code.
+// Additionally, printing bombBoard will not look clean due to use of null instead of ' ' - this should just be for debugging, not presentation.
+printBoard(bombBoard);
